5afc2050e5cc4818c705f07d5ff9abee479636eb
Matchstick15 <cody.sheedy@education.nsw.gov.au>
2025-08-25 09:50:06 +1000

    1

---
+++ Databases/SQLquery.sql
@@ -0,0 +1,8 @@
+--CREATE TABLE Users (
+--    StudentID INTEGER PRIMARY KEY AUTOINCREMENT,
+--    Forename TEXT NOT NULL,
+--    Surname  TEXT  NOT NULL,
+--    Subjects TEXT NOT NULL
+--);
+
+--INSERT into users (Forename, Surname, Subjects) VALUES ('cody', 'sheedy', 'math');
